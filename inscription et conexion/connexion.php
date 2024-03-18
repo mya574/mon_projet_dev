49dbf8068f9d49401622380753d6e59f1a20d842
Anthony Savouret <a.savouret@ecole-ipssi.net>
2024-03-18 11:00:03 +0100

    feat(controller): captcha

--- inscription et conexion/connexion.php
+++ inscription et conexion/connexion.php
@@ -2,47 +2,74 @@
 session_start();
 $error = "";
 
+// Check if the form for user login is submitted
 if (isset($_POST['identifiant']) && isset($_POST['mot_de_passe']) && isset($_POST['role'])) {
     $file_name = 'utilisateurs.csv';
     $file = fopen($file_name, 'r');
 
     if ($file) {
+        $valid_credentials = false;
         while (($line = fgetcsv($file)) !== false) {
             if (isset($line[2]) && isset($line[3]) && isset($line[4]) && $line[2] === $_POST['identifiant'] && $line[4] === $_POST['role']) {
                 if (password_verify($_POST['mot_de_passe'], $line[3])) {
-                    $_SESSION['identifiant'] = $_POST['identifiant'];
-                    $_SESSION['role'] = $_POST['role'];
-                    fclose($file);
-                    // Redirection en fonction du rôle
-                    switch ($_POST['role']) {
-                        case 'admin':
-                            header('Location: admin.php');
-                            break;
-                        case 'ecole':
-                            header('Location: ecole.php');
-                            break;
-                        case 'utilisateur':
-                            header('Location: user.php');
-                            break;
-                        default:
-                            // Redirection par défaut
-                            header('Location: index.php');
-                            break;
-                    }
-                    exit(); // Terminer le script après la redirection
+                    $valid_credentials = true;
+                    break;
                 } else {
                     $error = "Le mot de passe est incorrect.";
                 }
-            } else {
-                $error = "L'identifiant ou le rôle n'existe pas.";
             }
         }
         fclose($file);
+        if ($valid_credentials) {
+            $_SESSION['identifiant'] = $_POST['identifiant'];
+            $_SESSION['role'] = $_POST['role'];
+            // Redirect to index.php after successful login
+            header('Location: index.php');
+            exit();
+        } else {
+            $error = "L'identifiant ou le rôle est incorrect ou n'existe pas.";
+        }
     } else {
         $error = "Erreur lors de l'ouverture du fichier.";
     }
 }
+
+// Check if the form for sending additional data is submitted
+if (isset($_POST['submit'])) {
+    // Check if first name field is empty
+    if (empty($_POST['fname'])) {
+        echo "<h4>Veuillez entrer votre prénom</h4>";
+    }
+
+    // Check if last name field is empty
+    if (empty($_POST['lname'])) {
+        echo "<h4>Veuillez entrer votre nom</h4>";
+    }
+
+    // Check if reCAPTCHA response is empty
+    if (empty($_POST['g-recaptcha-response'])) {
+        echo "<h4>Résoudre le captcha</h4>";
+    }
+
+    // Validate reCAPTCHA
+    if (!empty($_POST["g-recaptcha-response"])) {
+        $secret = "6Ldx6ZkpAAAAABKP1aR4TKPd3S3-ZknB9qYdvH8s";
+
+        $response = file_get_contents('https://google.com/recaptcha/api/siteverify?secret=' . $secret . '&response=' . $_POST['g-recaptcha-response']);
+
+        $data = json_decode($response);
+
+        if ($data->success) {
+            // This message will only be displayed when reCAPTCHA validation succeeds
+            echo "<h2>Recaptcha successfully resolved, Data Sent</h2>";
+        } else 
+        {
+            echo "<h4>Please try again to solve the captcha</h4>";
+        }
+    }
+}
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -50,6 +77,7 @@ if (isset($_POST['identifiant']) && isset($_POST['mot_de_passe']) && isset($_POS
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Connexion</title>
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
 </head>
 <body>
 <div class="container">
@@ -70,6 +98,14 @@ if (isset($_POST['identifiant']) && isset($_POST['mot_de_passe']) && isset($_POS
         <input type="radio" id="utilisateur" name="role" value="utilisateur" required>
         <label for="utilisateur">Utilisateur</label>
         <input type="submit" value="Se connecter">
+    </form>
+    <!-- Form for additional data -->
+    <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+        Prénom <input type="text" name="fname"><br>
+        Nom <input type="text" name="lname"><br>
+        <!-- reCAPTCHA challenge -->
+        <div class="g-recaptcha" data-sitekey="6Ldx6ZkpAAAAAIF7eL6SKblN7Ft_FJtA7E8Oqyw_"></div>
+        <input type="submit" name="submit" value="Send Data">
     </form>
 </div>
 </body>
