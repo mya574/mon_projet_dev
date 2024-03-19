9a20cd1242fe6f0abd506ea0fa715ee91d5f337f
Ismo53 <zerboismael90@gmail.com>
2024-03-19 16:34:52 +0100

    feat (view):mise à jour utilisateur

--- Utilisateur/modifier_infos.php
+++ Utilisateur/modifier_infos.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Modifier Informations Utilisateur</title>
+    <title>Modifier vos informations</title>
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
